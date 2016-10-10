c6e8c9938feb22c7c79041d9464cf2d1754afa24
tumagonx <mr.tiar@gmail.com>
2016-10-11 02:32:34 +0700

    fix helpers for multiple input

--- subsysverXP.cmd
+++ subsysverXP.cmd
@@ -1,3 +1,6 @@
 @echo off
+:queue
 for %%I in ("pehdr-lite.exe") do if exist "%%~$PATH:I" if exist "%~1" pehdr-lite "%~dpnx1" -subsysver 5.1
+shift /1
+if not "%1" EQU "" goto :queue
 pause
--- unsignDS.cmd
+++ unsignDS.cmd
@@ -1,3 +1,6 @@
 @echo off
-for %%I in ("osslsigncode.exe") do if exist "%~1" osslsigncode remove-signature -in "%~dpnx1" -out "%~dpnx1"
+:queue
+for %%I in ("osslsigncode.exe") do if exist "%%~$PATH:I" if exist "%~1" osslsigncode remove-signature -in "%~dpnx1" -out "%~dpnx1"
+shift /1
+if not "%1" EQU "" goto :queue
 pause
