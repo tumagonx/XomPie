0e78c4b3855cce3c1b12c6be888516974d2d6730
tumagonx <mr.tiar@gmail.com>
2016-10-11 04:30:08 +0700

    SendTo helper for replacing string in binary

---
+++ replaceDLL.cmd
@@ -0,0 +1,8 @@
+@echo off
+:: this action completely dubious, please backup before proceed
+:: at the moment just kernel32.dll
+:queue
+for %%I in ("gsar.exe") do if exist "%%~$PATH:I" if exist "%~1" gsar -o -i -skernel32.dll -rkernelxp.dll "%~dpnx1"
+shift /1
+if not "%1" EQU "" goto :queue
+pause
