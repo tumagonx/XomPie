f2dd1bcfb9df856e73b3afc498a0106dd07b1600
tumagonx <mr.tiar@gmail.com>
2016-10-13 01:15:59 +0700

    don't use unsigner

--- xpatcher.bat
+++ xpatcher.bat
@@ -1,5 +1,4 @@
 @echo off
-for %%I in ("osslsigncode.exe") do if not exist "%%~$PATH:I" echo Error: osslsigncode not found& goto :EOF
 for %%I in ("gsar.exe") do if not exist "%%~$PATH:I" echo Error: gsar not found& goto :EOF
 for %%I in ("pehdr-lite.exe") do if not exist "%%~$PATH:I" echo Error: pehdr-lite not found& goto :EOF
 if not exist "%~1\*" echo Error: not a directory& goto :EOF
@@ -12,7 +11,6 @@ for /f "delims=/ usebackq" %%F in (`dir *.exe *.dll /s /a-d /b /oen`) do (
 echo.
 echo Patching "%%~nxF"...
 attrib -r "%%~F" && copy /y "%%~F" "%%~dpnxF.bak" >nul 2>&1
-osslsigncode remove-signature -in "%%~dpnxF.bak" -out "%%~dpnxF"
 gsar -o -i -s:x00kernel32.dll:x00 -r:x00kernelxp.dll:x00 "%%~dpnxF"
 gsar -o -i -s:x00advapi32.dll:x00 -r:x00advapixp.dll:x00 "%%~dpnxF"
 pehdr-lite "%%~dpnxF" -subsysver 5.1
