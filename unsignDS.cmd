28bd72a3d16bd1539c13d98cef95ce5f662350e1
tumagonx <mr.tiar@gmail.com>
2016-10-12 17:35:58 +0700

    remove readonly attribute before backup

--- unsignDS.cmd
+++ unsignDS.cmd
@@ -1,7 +1,7 @@
 @echo off
 :queue
-if exist "%~1" copy /y "%~1" "%~dpn1.bak" >nul 2>&1
-for %%I in ("osslsigncode.exe") do if exist "%%~$PATH:I" osslsigncode remove-signature -in "%~dpn1.bak" -out "%~dpnx1"
+if exist "%~1" attrib -r "%~1" & copy /y "%~1" "%~dpnx1.bak" >nul 2>&1
+for %%I in ("osslsigncode.exe") do if exist "%%~$PATH:I" osslsigncode remove-signature -in "%~dpnx1.bak" -out "%~dpnx1"
 shift /1
 if not "%1" EQU "" goto :queue
 pause
