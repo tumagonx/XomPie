6d6d06922b9b7ee3c118ce8776f206b6a03ce770
tumagonx <mr.tiar@gmail.com>
2016-10-12 17:35:20 +0700

    add advapi32 into replacement

--- replaceDLL.cmd
+++ replaceDLL.cmd
@@ -2,7 +2,10 @@
 :: this action completely dubious, please backup before proceed
 :: at the moment just kernel32.dll
 :queue
-for %%I in ("gsar.exe") do if exist "%%~$PATH:I" if exist "%~1" gsar -o -i -skernel32.dll -rkernelxp.dll "%~dpnx1"
+for %%I in ("gsar.exe") do if exist "%%~$PATH:I" if exist "%~1" (
+gsar -o -i -skernel32.dll -rkernelxp.dll "%~dpnx1"
+gsar -o -i -sadvapi32.dll -radvapixp.dll "%~dpnx1"
+)
 shift /1
 if not "%1" EQU "" goto :queue
 pause
