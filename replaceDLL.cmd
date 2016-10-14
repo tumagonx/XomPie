22be4e3572f4c39e8d967144c933f65e8ec23bfc
tumagonx <mr.tiar@gmail.com>
2016-10-15 01:43:26 +0700

    IAT entry may padded with NOP

--- replaceDLL.cmd
+++ replaceDLL.cmd
@@ -5,6 +5,8 @@
 for %%I in ("gsar.exe") do if exist "%%~$PATH:I" if exist "%~1" (
 gsar -o -i -s:x00kernel32.dll:x00 -r:x00kernelxp.dll:x00 "%~dpnx1"
 gsar -o -i -s:x00advapi32.dll:x00 -r:x00advapixp.dll:x00 "%~dpnx1"
+gsar -o -i -s:x90kernel32.dll:x00 -r:x00kernelxp.dll:x00 "%~dpnx1"
+gsar -o -i -s:x90advapi32.dll:x00 -r:x00advapixp.dll:x00 "%~dpnx1"
 )
 shift /1
 if not "%1" EQU "" goto :queue
