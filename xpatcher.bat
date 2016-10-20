bbacf6e4222b4f81f8d87468c87deefd1ed5120e
tumagonx <mr.tiar@gmail.com>
2016-10-21 05:46:00 +0700

    ready for 0.1a

--- xpatcher.bat
+++ xpatcher.bat
@@ -15,10 +15,13 @@ echo Patching "%%~nxF"...
 attrib -r "%%~F" && copy /y "%%~F" "%%~dpnxF.bak" >nul 2>&1
 gsar -o -i -s:x00kernel32.dll:x00 -r:x00kernelxp.dll:x00 "%%~dpnxF"
 gsar -o -i -s:x90kernel32.dll:x00 -r:x90kernelxp.dll:x00 "%%~dpnxF"
-gsar -o -i -s:x00k:x00e:x00r:x00n:x00e:x00l:x003:x002:x00.:x00d:x00l:x00l:x00 -r:x00k:x00e:x00r:x00n:x00e:x00l:x00x:x00p:x00.:x00d:x00l:x00l:x00 "%%~dpnxF"
+gsar -o -i -s:x00k:x00e:x00r:x00n:x00e:x00l:x003:x002:x00.:x00d:x00l:x00l:x00:x00 -r:x00k:x00e:x00r:x00n:x00e:x00l:x00x:x00p:x00.:x00d:x00l:x00l:x00:x00 "%%~dpnxF"
 gsar -o -i -s:x00advapi32.dll:x00 -r:x00advapixp.dll:x00 "%%~dpnxF"
 gsar -o -i -s:x90advapi32.dll:x00 -r:x90advapixp.dll:x00 "%%~dpnxF"
-gsar -o -i -s:x00a:x00d:x00v:x00a:x00p:x00i:x003:x002:x00.:x00d:x00l:x00l:x00 -r:x00a:x00d:x00v:x00a:x00p:x00i:x00x:x00p:x00.:x00d:x00l:x00l:x00 "%%~dpnxF"
+gsar -o -i -s:x00a:x00d:x00v:x00a:x00p:x00i:x003:x002:x00.:x00d:x00l:x00l:x00:x00 -r:x00a:x00d:x00v:x00a:x00p:x00i:x00x:x00p:x00.:x00d:x00l:x00l:x00:x00 "%%~dpnxF"
+gsar -o -i -s:x00msvcrt.dll:x00 -r:x00xpvcrt.dll:x00 "%%~dpnxF"
+gsar -o -i -s:x90xpvcrt.dll:x00 -r:x90xpvcrt.dll:x00 "%%~dpnxF"
+gsar -o -i -s:x00m:x00s:x00v:x00c:x00r:x00t:x00.:x00d:x00l:x00l:x00:x00 -r:x00x:x00p:x00v:x00c:x00r:x00t:x00.:x00d:x00l:x00l:x00:x00 "%%~dpnxF"
 pehdr-lite "%%~dpnxF" -subsysver 5.1
 )
 echo Finished...
