97703c90186a5277955c63ce3c46a98093e2d216
tumagonx <mr.tiar@gmail.com>
2016-11-18 16:23:05 +0700

    too many (!)

--- xominstall.cmd
+++ xominstall.cmd
@@ -1,4 +1,7 @@
 @echo off
+echo STOP: ONLY CONTINUE IF YOU KNOW WHAT YOU DO AND HAVE TESTED THE INSTALLER ON VM!
+echo Enter to continue...
+pause>nul 2>&1
 :queue
 for %%I in ("msiexec7.exe") do if exist "%%~$PATH:I" if exist "%~1" if /i "%~x1" equ ".msi" msiexec7 /i "%~dpnx1"
 shift /1
