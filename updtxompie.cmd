7426ada4cbde31fd8aa473cb233411c1719acebc
tumagonx <mr.tiar@gmail.com>
2017-07-15 08:46:32 +0700

    forgot ws2_32

--- updtxompie.cmd
+++ updtxompie.cmd
@@ -1,9 +1,9 @@
 @echo off
 :: at the moment this is only system files updater
 for %%I in ("dwnl.exe") do if not exist "%%~$PATH:I" echo Error: dwnl not found&pause & goto :EOF
-echo UPDATING XOMPIE DLL AND PATCHER:
+echo UPDATING XOMPIE DLL:
 pause
-set files=advapixp kernelxp xpvcrt shellxp userxp gdixp dxgi dwrite d3d11 d3d10core d3d10_1 d3d10 d2d1 d3dx winetxc wined3d dwmapi wer wevtapi
+set files=advapixp kernelxp xpvcrt shellxp userxp gdixp dxgi dwrite d3d11 d3d10core d3d10_1 d3d10 d2d1 d3dx winetxc wined3d dwmapi wer wevtapi ws2_32
 call :XOMDWNL %files%
 
 echo.
